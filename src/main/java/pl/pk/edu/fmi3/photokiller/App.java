95926f15f3fccaa22c0f47b65c8d51bc3574eaf6
Michał Nosek <michal.nosek@velis.pl>
2014-12-14 15:12:42 +0100

    Initial commit

---
+++ src/main/java/pl/pk/edu/fmi3/photokiller/App.java
@@ -0,0 +1,13 @@
+package pl.pk.edu.fmi3.photokiller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pl/pk/edu/fmi3/photokiller/main/MainTest.java
@@ -0,0 +1,7 @@
+package pl.pk.edu.fmi3.photokiller.main;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+}
